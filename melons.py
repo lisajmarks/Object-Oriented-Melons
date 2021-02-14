1407de6e612ad54f21319212b913c9dd3fc902d0
Lisa Yang <lisajmarks@protonmail.com>
2021-02-14 00:26:35 +0000

    Added Government Melon Class

--- melons.py
+++ melons.py
@@ -4,14 +4,13 @@ class AbstractMelonOrder():
     """Abstract melon order"""
 
     def __init__(self, species, qty, order_type, tax):
+        """Initialize attributes"""
 
         self.species = species
         self.qty = qty
         self.order_type = order_type
         self.tax = tax
-        self.shipped = False
-
-        """Initialize attributes"""
+        self.shipped = False      
 
 
 
@@ -31,7 +30,14 @@ class DomesticMelonOrder(AbstractMelonOrder):
         """Calculate price, including tax."""
 
         base_price = 5
+        
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
+            
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.species == "International" and self.qty < 10: 
+            total += 3 
 
         return total
 
@@ -71,3 +77,15 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    class GovernmentMelonOrder(AbstractMelonOrder):
+        """Government is buying melons"""
+
+        def __init__(self, species, qty):
+            super().__init__(species, qty, "government", 0.0)
+            self.passed_inspection = False 
+
+        def mark_inspection(self, passed):
+            """Record wheter the melon order has passed inspection"""
+            self.passed_inspection = passed
+        
--- salespeople.py
+++ salespeople.py
@@ -29,3 +29,4 @@ class InternSalesperson(Salesperson):
 
         self.base_salary = base_salary
         self.commission_rate = commission_rate
+
